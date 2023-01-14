acd88cf0310071ed1b1b4b2f573ca4898ef2cc88
Adrián Fernández García <adrianfgdev@gmail.com>
2023-01-14 15:48:55 +0100

    Init

---
+++ SpringWebApp/SpringWebApp/src/test/java/adfer/springapp/SpringWebApp/SpringWebAppApplicationTests.java
@@ -0,0 +1,13 @@
+package adfer.springapp.SpringWebApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringWebAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
